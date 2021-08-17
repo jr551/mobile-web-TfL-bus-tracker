3d4df22f5b1781d2361d4809a6dee27d17be89b9
jr_blue_551 <johnrowe551@gmail.com>
2021-08-17 20:16:50 +0100

    Initial Commit
    Initial Commit

---
+++ bus.js
@@ -0,0 +1,197 @@
+var selectedNaptan = "";
+var selectedName = "";
+var firstPageLoad = true;
+
+var myIconnew = L.icon(
+{
+    iconUrl: 'busstopnew.png',
+    iconSize: [32, 51],
+    iconAnchor: [16, 51],
+    popupAnchor: [-3, -76],
+});
+var myIcon = L.icon(
+{
+    iconUrl: 'busstopicon.png',
+    iconSize: [32, 51],
+    iconAnchor: [16, 51],
+    popupAnchor: [-3, -76],
+});
+var myIcon2 = L.icon(
+{
+    iconUrl: 'busstopicon2.png',
+    iconSize: [32, 51],
+    iconAnchor: [16, 51],
+    popupAnchor: [-3, -76],
+});
+var mapMarkers = [];
+
+$(function ()
+{
+    $("#accordion").accordion(
+    {
+        active: false,
+        collapsible: true,
+        heightStyle: "content"
+    });
+});
+$(function ()
+{
+    $("#bstop").selectmenu();
+});
+
+function changeBackgroundStatusA()
+{
+    document.body.style.background = "#e6ffe6";
+}
+
+function changeBackgroundStatusB()
+{
+    document.body.style.background = "#ffddcc";
+}
+
+function refreshBusses()
+{
+    changeBackgroundStatusA();
+    var j;
+    for (j = 0; j < 21; j++)
+    {
+        document.getElementById("bus_" + j + "_title").style.visibility = 'hidden';
+        document.getElementById("bus_" + j + "_p").style.visibility = 'hidden';
+    }
+    if (selectedNaptan < 3)
+    {
+        changeBackgroundStatusB();
+        return;
+    }
+
+
+    $.getJSON("https://api.tfl.gov.uk/StopPoint/" + selectedNaptan + "/arrivals", function (data)
+    {
+        document.getElementById("bus_selected").innerHTML = selectedName;
+        var busses_due = data.length;
+        document.getElementById("bus_summary").innerHTML = "Busses found: " + busses_due;
+
+        //sort by arrival time
+        data.sort(function (a, b)
+        {
+            return (a["timeToStation"] > b["timeToStation"]) ? 1 : ((a["timeToStation"] < b["timeToStation"]) ? -1 : 0);
+        });
+
+        var i;
+        for (i = 0; i < busses_due; i++)
+        {
+		
+		document.getElementById("bus_"+i+"_title").style.visibility = 'visible';
+		document.getElementById("bus_"+i+"_p").style.visibility = 'visible';
+		
+		var mins =  Math.round(parseInt(data[i]["timeToStation"]) / 60) ;
+		document.getElementById("bus_"+i+"_title").innerHTML="<span style='color:red;'>" +data[i]["lineName"] + "</span> to <i>" + data[i]["destinationName"] + "</i> <b>" + mins + "</b> mins";
+		document.getElementById("bus_"+i+"_p").innerHTML= "This bus stop: <b>" + data[i]["stationName"] +
+												"<br> </b> Number Plate:  <b><span style='background-color:yellow'>" + data[i]["vehicleId"] + 
+												"</span><br></b>Bus Stop Code:  <b>" +data[i]["platformName"] + "</b>" + 
+												"<br></b> Going to:  <b>" +data[i]["destinationName"] +"</b>";
+												
+        }
+        changeBackgroundStatusB();
+    })
+
+}
+
+window.onload = function ()
+{
+    changeBackgroundStatusB();
+    getLocation();
+    changeBackgroundStatusB();
+    refreshBusses();
+
+};
+window.setInterval(function ()
+{
+    refreshBusses();
+}, 9000);
+
+
+function getLocation()
+{
+    if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else
+    {
+    }
+}
+
+function showPosition(position)
+{
+
+    var mymap = L.map('mapid').setView([position.coords.latitude, position.coords.longitude], 17);
+    
+    
+    //Please get your own api key from mapbox dont use mine all up!
+    //Please get your own api key from mapbox dont use mine all up!
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9obmJsdWV3b2xmZ2FtZXMiLCJhIjoiY2tzZ2VscTRxMWd2NTJ3bzRhYW40ZHY0eCJ9.RsknaU9ElEOkesW1g0YqGw', {
+        maxZoom: 18,
+        
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mymap);
+
+    var circle = L.circle([position.coords.latitude, position.coords.longitude], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: position.coords.accuracy
+    }).addTo(mymap);
+    $.getJSON("getbusstop.php?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude, function (data)
+    {
+
+        var i;
+        for (i = 0; i < 45; i++)
+        {
+            var marker = L.marker([data[i]["lat"], data[i]["lon"]], {
+                icon: myIcon
+            }).addTo(mymap);
+
+            mapMarkers[mapMarkers.length] = marker;
+            marker.naptan = data[i]["naptan"];
+            marker.bname = data[i]["name"];
+            marker.bindPopup(data[i]["name"]);
+
+            function onMapClick(e)
+            {
+                selectedNaptan = e.sourceTarget.naptan;
+                selectedName = e.sourceTarget.bname;
+                for (var j = 0; j < mapMarkers.length; j++)
+                {
+                    mapMarkers[j].setIcon(myIcon);
+                }
+                document.getElementById("bus_selected").innerHTML = "Loading";
+                document.getElementById("bus_summary").innerHTML = "Loading";
+
+                e.sourceTarget.setIcon(myIcon2);
+                refreshBusses();
+            }
+            marker.on('click', onMapClick);
+        }
+
+        if (firstPageLoad)
+        {
+            selectedNaptan = mapMarkers[0].naptan;
+            selectedName = mapMarkers[0].bname;
+            for (var j = 0; j < mapMarkers.length; j++)
+            {
+                mapMarkers[j].setIcon(myIcon);
+
+            }
+            mapMarkers[0].setIcon(myIcon2);
+            refreshBusses();
+            firstPageLoad = false;
+        }
+        changeBackgroundStatusB();
+
+
+    });
+
+}
---
+++ getbusstop.php
@@ -0,0 +1,41 @@
+<?php
+
+function array_sort_by_column(&$arr, $col, $dir = SORT_ASC) {
+    $sort_col = array();
+    foreach ($arr as $key=> $row) {
+        $sort_col[$key] = $row[$col];
+    }
+
+    array_multisort($sort_col, $dir, $arr);
+}
+function distance($lat1, $lon1, $lat2, $lon2) {
+
+  $theta = $lon1 - $lon2;
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  $miles = $dist * 60 * 1.1515;
+  $unit = strtoupper($unit);
+  return $miles;
+}
+
+// Store this in a DB for fast access this is not the way to do it below.
+$bus_stops = array();
+if (($handle = fopen('bus_stops_long_lat.csv', 'r')) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) { 
+        
+		
+		$bus_stops[] = array("distance"=>distance($data[2],$data[3],$_GET['lat'],$_GET['lon']),
+							"naptan"   =>$data[0], 
+							"name"     =>$data[1],
+							"lat"     =>$data[2],
+							"lon"     =>$data[3]
+							);
+    }
+    fclose($handle);
+}
+
+array_sort_by_column($bus_stops, "distance");
+echo json_encode(array_slice($bus_stops,0,45));
+
+?>
